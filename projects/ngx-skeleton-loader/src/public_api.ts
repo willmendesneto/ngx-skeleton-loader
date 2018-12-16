fddd369c31357a87547ac8cc3f4a8323354b188f
Will Mendes <willmendesneto@gmail.com>
2018-12-16 23:48:39 +1100

    feat: decreasing bundle size

--- projects/ngx-skeleton-loader/src/public_api.ts
+++ projects/ngx-skeleton-loader/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-skeleton-loader
  */
 
-export * from './lib/ngx-skeleton-loader.service';
 export * from './lib/ngx-skeleton-loader.component';
 export * from './lib/ngx-skeleton-loader.module';
