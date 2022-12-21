ffe0d77644d2aae8514a57b6f55e54fb84cbee48
chimurai <655241+chimurai@users.noreply.github.com>
2022-12-21 23:34:18 +0000

    build: bump minimal node v14.20.0

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [process.env.CI ? 'ChromeHeadlessNoSandbox': 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
