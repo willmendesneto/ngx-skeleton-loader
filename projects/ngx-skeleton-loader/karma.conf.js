f0471dc0bd0644a6505d83b64748677c0f207a7b
chimurai <655241+chimurai@users.noreply.github.com>
2023-02-02 20:05:18 +0100

    build: update to angular v15 (#131)
    * build: update to angular v14
    
    - npx browserslist@latest --update-db
    - npx ng update @angular/core@14 @angular/cli@14 @angular-eslint/schematics@14 @nguniversal/express-engine@14 @nguniversal/builders@14 --force
    
    * build: update to angular v15
    
    - npm i -D ajv@latest core-js@latest
    - npx ng update @angular/core@15 @angular/cli@15 @angular-eslint/schematics@15 @nguniversal/express-engine@15 @nguniversal/builders@15 --force
    
    * ci: disable Angular CLI Analytics
    
    * build: bump minimal node v14.20.0
    
    * ci: remove sudo usage
    
    - sudo usage prevented ENVIRONMENT variables being passed to scripts

--- projects/ngx-skeleton-loader/karma.conf.js
+++ projects/ngx-skeleton-loader/karma.conf.js
@@ -26,7 +26,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [process.env.CI ? 'ChromeHeadlessNoSandbox': 'Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
