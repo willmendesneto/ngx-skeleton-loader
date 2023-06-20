0367459b7991055a5ea93fb98aee3a88ddf54b4c
chimurai <655241+chimurai@users.noreply.github.com>
2023-06-20 17:22:40 +0200

    build: update to angular 16 (#147)
    * build: update to angular 16
    
    - npx ng update @angular/core@16 @angular/cli@16 @angular-eslint/schematics@16 @nguniversal/express-engine@16 @nguniversal/builders@16 --force
    - remove npm postinstall ngcc
    - remove .browserslistrc
    
    * ci: bump to node v18.16.0
    
    * lint: fix @typescript-eslint errors
    
    * ci(bundlesize): fix ngx-skeleton-loader/fesm2022/ngx-skeleton-loader.mjs
    
    * fix(build:ssr): no exported member 'renderModuleFactory'
    
    * ci: allow node v18 and higher

--- projects/ngx-skeleton-loader/src/lib/ngx-skeleton-loader-config.types.ts
+++ projects/ngx-skeleton-loader/src/lib/ngx-skeleton-loader-config.types.ts
@@ -5,7 +5,6 @@ export type NgxSkeletonLoaderConfigTheme = {
   extendsFromRoot?: boolean;
   // This is required since ngStyle is using `any` as well
   // More details in https://angular.io/api/common/NgStyle
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 } | null;
 
--- projects/ngx-skeleton-loader/src/lib/ngx-skeleton-loader.module.spec.ts
+++ projects/ngx-skeleton-loader/src/lib/ngx-skeleton-loader.module.spec.ts
@@ -19,7 +19,6 @@ import { NgxSkeletonLoaderModule } from './ngx-skeleton-loader.module';
 class ContainerComponent {}
 
 describe('NgxSkeletonLoaderModule method', () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let fixture: any;
 
   beforeEach(waitForAsync(() => {
